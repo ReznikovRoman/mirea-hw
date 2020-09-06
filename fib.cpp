e90d03350cc8ab253187efcd5f814340baceb566
Reznikov Roman <rereznikov2002@gmail.com>
2020-09-06 22:02:35 +0300

    Add Fibonacci task

---
+++ fib.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int n)
+{
+    if (n <= 1) { return n; }
+
+    return fib(n-1) + fib(n-2);
+}
+
+
+int main()
+{
+
+    int n;
+    cout << endl << "\tFibonacci sequence" << endl;
+
+    cout << "\tEnter 'n': ";
+    cin >> n;
+    
+    cout << endl << "\tFib_n: " << fib(n) << endl;
+}
