21c53ad0dc789e0b6dd2ac36e893b15803bd6f28
Reznikov Roman <rereznikov2002@gmail.com>
2020-09-10 16:57:54 +0300

    Add sorted words (task-41)

---
+++ exam_preparation/files/sorted_words_41.cpp
@@ -0,0 +1,78 @@
+
+
+#include <iostream>
+
+#include <vector>
+
+#include <algorithm>
+
+#include <string>
+using namespace std;
+
+
+vector<string> splitString(string s)
+{
+    vector<string> words;
+    size_t pos = s.find(' ');
+    size_t initialPos = 0;
+    
+    while (pos != string::npos)
+    {
+        words.push_back(s.substr(initialPos, pos - initialPos));
+        initialPos = pos +  1;
+
+        pos = s.find(' ', initialPos);  
+    }
+
+    words.push_back(s.substr(initialPos, min(pos, s.size()) - initialPos + 1));
+
+    return words;
+}
+
+
+bool compareLength(string i, string j)
+{
+    return i.size() < j.size();
+}
+
+void sortByLength(vector<string>& words)
+{
+    sort(words.begin(), words.end(), compareLength);
+}
+
+
+void printWords(vector<string> words)
+{
+    for (auto word : words) { cout << word << endl; }
+}
+
+
+int main()
+{
+    string S = "";
+    int n;
+    vector<string> words;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << "Enter word: ";
+        string word;
+        cin >> word;
+        S += word + " ";
+    }
+    
+    S.resize(S.size() - 1);
+
+    words = splitString(S);
+
+    sortByLength(words);
+
+    cout << endl << endl;
+    cout << "Sorted words (by length):" << endl;
+    printWords(words);
+
+    return 0;
+}
