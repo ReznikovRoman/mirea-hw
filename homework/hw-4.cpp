ce53c4c9c7b1a314d9cb6c51546179ac04c0d03f
ReznikovRoman <romanreznikov2002@yandex.ru>
2020-09-16 23:00:44 +0300

    Add tasks-2,3,9 in to the hw-4

--- homework/hw-4.cpp
+++ homework/hw-4.cpp
@@ -5,13 +5,17 @@
 #include <fstream>
 #include <string>;
 
+#include <algorithm>
 using namespace std;
+
+
+const float  PI = 3.14159265358979;
 
 
 void task1()
 {
 
-    cout << endl << "Task - 1" << endl;
+    cout << endl << endl << "Task - 1" << endl;
 
     string line;
     int sum = 0;
@@ -41,13 +45,62 @@ void task1()
     cout << endl << "Sum: " << sum << endl;
 }
 
+
+int sign(const int &x)
+{
+    if (x > 0) { return 1; }
+    if (x < 0) { return -1; }
+    return 0;
+}
+
 void task2()
 {
+    cout << endl << endl << "Task - 2" << endl;
 
+    int x;
+    cout << "Enter x: ";
+    cin >> x;
+
+    cout << endl << "Sign X: " << sign(x) << endl;
+}
+
+
+int rectangleArea(const int &width, const int &height)
+{
+    return width * height;
+}
+
+float triangleArea(const float &base, const float &height)
+{
+    return (base*height) / 2;
+}
+
+float circleArea(const float &r)
+{
+    return PI * r*r ;
 }
 
 void task3()
 {
+    cout << endl << endl << "Task - 3" << endl;
+
+    int width, height;
+    cout << "Enter width and height of a rectangle: ";
+    cin >> width >> height;
+    cout << "Rectangle area is: " << rectangleArea(width, height) << endl;
+    cout << endl;
+
+    float base, tHeight;
+    cout << "Enter base and verical height of a triangle: ";
+    cin >> base >> tHeight;
+    cout << "Triangle area is: " << triangleArea(base, tHeight) << endl;
+    cout << endl;
+
+    float r;
+    cout << "Enter radius of a circle, r: ";
+    cin >> r;
+    cout << "Circle area is: " << circleArea(r) << endl;
+    cout << endl;
 
 }
 
@@ -71,14 +124,91 @@ void task7()
 
 }
 
-void task8()
+void task8()  // умножение матриц
 {
 
 }
 
-void task9()
+
+int toDigit(const char &c)  // переводим текущий символ в число
+{
+    if (c >= '0' && c <= '9') { return (int)c - '0'; }
+    else { return (int)c - 'A' + 10; }
+}
+
+char toChar(const int &num)  // переводим текущее число в строку
+{
+    if (num >= 0 && num <= 9) { return (char)(num + '0'); }
+    else { return (char)(num - 10 + 'A'); }
+}
+
+
+int toDecimal(string s, const int &base)
 {
 
+    /*
+        1. Умножаем каждую цифру числа на основание текущей с.с. с учетом веса текущего разряда
+        2. Записываем сумму полученных произведений
+    */
+
+    int num = 0;
+    int power = 1;
+
+    for (int i = s.length() - 1; i >= 0; i--)
+    {
+        if (toDigit(s[i]) >= base) // если пользователь ввел неверное число (есть цифры >= основанию с.с.)
+        {
+            cout << "Invalid number!";
+            return -1;
+        }
+
+        num += toDigit((s[i])) * power;
+        power *= base;
+    }
+
+    return num;
+}
+
+string fromDecimal(int num, const int &base)
+{
+
+    /*
+       1. Делим 10-ичное число на base(заданную с.с.), запоминаем остатки от деления
+       2. Переворачиваем получившиеся остатки
+    */
+
+    int index = 0;
+    string ans;
+
+    while (num > 0)
+    {
+        ans += toChar(num % base);
+        num /= base;
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    return ans;
+
+}
+
+void task9()  // системы счисления
+{
+    cout << endl << endl << "Task - 9" << endl;
+
+    /*
+    1. Переводим число в 10-ичную систему счисления
+    2. Из 10-ичной с.с. переводим в заданную с.с.
+    */
+
+    string userNum;
+    int currBase, newBase;
+    cout << "Enter number, current base and new base: ";
+    cin >> userNum >> currBase >> newBase;
+
+
+    //cout << endl << "Decimal value: " << toDecimal(userNum, currBase) << endl;
+    cout << endl << "In base " << newBase << " : " << fromDecimal(toDecimal(userNum, currBase), newBase) << endl;
 }
 
 
@@ -86,23 +216,23 @@ void task9()
 int main()
 {
 
-    task1();
+    task1();  // файл
 
-    //task2();
+    task2();  // знак числа
 
-    //task3();
+    task3();  // геометрические фигуры
 
-    //task4();
+    //task4();  // былая слава
 
-    //task5();
+    //task5();  // синусоида
 
-    //task6();
+    //task6();  // автоматный распознаватель
 
-    //task7();
+    //task7();  // генератор случайных чисел
 
-    //task8();
+    //task8();  // умножение матриц
 
-    //task9();
+    task9();  // системы счисления
 
     return 0;
 }
