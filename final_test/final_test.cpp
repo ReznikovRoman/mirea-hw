ddd4e83e68c70ec782d252492aee63935f532cc8
ReznikovRoman <romanreznikov2002@yandex.ru>
2020-12-17 18:58:45 +0300

    Add task3,4 to the final test

--- final_test/final_test.cpp
+++ final_test/final_test.cpp
@@ -51,8 +51,8 @@ void task2()
     n4 = m % 3;  // m % 3 == 1 ==> n4 = 1 ||||| m % 3 == 2 ==> n4 = 2
     n3 = (m - 4 * n4) / 3; // оставшиеся лопасти идут на трехлопастные спиннеры
 
-    if (n3 >= 0) { cout << "n3: " << n3 << endl << "n4: " << n4 << endl; }
-    else { cout << 0 << endl << 0; }
+    if (n3 >= 0) cout << "n3: " << n3 << endl << "n4: " << n4 << endl;
+    else cout << 0 << endl << 0;
 
     cout << endl;
 }
@@ -62,17 +62,72 @@ void task3()
 {
     cout << endl << "3 - Not about spinners" << endl;
 
+    int n, m, ansX = 0, ansY = 0;
+    cin >> n;
+    cin >> m;
 
+    for (int x1 = 0; x1 < n; x1++)
+    {
+        for (int x2 = x1; x2 < n; x2++)
+        {
+            ansX += 1;
+        }
+    }
+
+    for (int y1 = 0; y1 < m; y1++)
+    {
+        for (int y2 = y1; y2 < m; y2++)
+        {
+            ansY += 1;
+        }
+    }
+
+    cout << ansX * ansY;
 
     cout << endl;
 }
 
 
+/*
+* Находит номер купе по номеру свободного места
+*/
+int section(int k)
+{
+    if (k <= 36) return (k - 1) / 4;
+    return 8 - (k - 37) / 2;
+}
+
 void task4()
 {
     cout << endl << "4 - Train carriage" << endl;
 
+    int count[9] = { }; // число свободных мест в i-ом купе
+    int n;
+    int ans = 0, curr = 0;
+    cin >> n;
 
+    for (int i = 0; i < n; i++)
+    {
+        int fp;
+        cin >> fp;
+        count[section(fp)] += 1;
+    }
+
+    // находим самое большое число подряд идущих 6 (т.е. свободных купе)
+    for (int i = 0; i < 9; i++)
+    {
+        if (count[i] == 6)
+        {
+            curr += 1;
+            ans = max(ans, curr);
+        }
+        else
+        {
+            curr = 0;
+        }
+    }
+
+    cout << ans;
 
     cout << endl;
 }
@@ -92,15 +147,15 @@ void task5()
 int main()
 {
 
-    task1(); // задача 1 - спиннеры
+    task1(); // задача 1 - спиннеры - DONE
 
-    task2(); // задача 2 - снова спиннеры
+    task2(); // задача 2 - снова спиннеры - DONE
 
-    task3(); // задача 3 - не про спиннеры
+    task3(); // задача 3 - не про спиннеры - DONE
 
-    task4(); // задача 4 - плацкартный вагон
+    task4(); // задача 4 - плацкартный вагон - DONE
 
-    task5(); // задача 5 - кинотеатр
+    // task5(); // задача 5 - кинотеатр
 
 
     return 0;
