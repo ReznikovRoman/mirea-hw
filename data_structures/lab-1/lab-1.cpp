19fa09f2b10fc61a603cee7ae916eb74b18848f5
ReznikovRoman <romanreznikov2002@yandex.ru>
2021-03-11 18:47:17 +0300

    Finish task-1.1

--- data_structures/lab-1/lab-1.cpp
+++ data_structures/lab-1/lab-1.cpp
@@ -1,28 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <array>
 
 using namespace std;
 
-
-void printVector(const vector<int> & v)
+void printArr(int *arr, const int &n)
 {
 	cout << endl;
 
-	for (int el : v)
-	{
-		cout << el << " ";
-	}
-
-	cout << endl;
-}
-
-
-void printArr(int *arr, const int & n)
-{
-	cout << endl;
-
-	for (size_t i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << " ";
 	}
@@ -30,53 +14,43 @@ void printArr(int *arr, const int & n)
 	cout << endl;
 }
 
-
-void populateArr(int* arr, const int & n)
+void populateArr(int *arr, const int &n)
 {
-	for (size_t i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand() % 2 + 1;
+		arr[i] = rand() % 10 + 1;
 	}
 }
 
-
-void delFirstMethod(int *x, int n, const int & key)
+void delFirstMethod(int *x, int &n, const int &key)
 {
 	int i = 0;
+	int actionsCount = 0;
 
-	while (i <= n)
+	while (i < n)
 	{
+		actionsCount++;
 		if (x[i] == key)
 		{
 			for (int j = i; j < n - 1; j++)
-			{
-				cout << x[j] << " " << x[j + 1] << endl;
 				x[j] = x[j + 1];
-			}
-
 			n -= 1;
+			actionsCount++;
 		}
 		else
 		{
 			i += 1;
 		}
 	}
-
-	
+	cout << "\nActions (permutations, deletions) count: " << actionsCount;
 }
-
 
 void task1()
 {
-	// vector<int> v = {1, 2, 3, 4, 5, 6, 7};
-
-	// delFirstMethod(v, v.size(), 2);
-
-	// printVector(v);
 
 	int n = 5;
-	int key = 2;
-	int* arr = new int[n];
+	int key = 1;
+	int *arr = new int[n];
 
 	populateArr(arr, n);
 	printArr(arr, n);
@@ -86,16 +60,12 @@ void task1()
 	printArr(arr, n);
 }
 
-
 void task2()
 {
-
 }
-
 
 int main()
 {
 	task1();
 	// task2();
 }
-
