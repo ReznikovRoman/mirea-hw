cf0f7cac980880f091fd37cc5a69832b5fbd8eb9
ReznikovRoman <romanreznikov2002@yandex.ru>
2021-03-11 19:05:59 +0300

    Finish task-1.2

--- data_structures/lab-1/lab-1.cpp
+++ data_structures/lab-1/lab-1.cpp
@@ -62,10 +62,52 @@ void task1()
 
 void task2()
 {
+	cout << "\nTask - 1.2 (#6)" << endl;
+
+	const int n = 3, m = 3;
+	int matrix[n][m];
+	int maxValue = -1000;
+
+	// populate matrix
+	for (size_t i = 0; i < n; i++)
+	{
+		for (size_t j = 0; j < m; j++)
+		{
+			cout << "Enter value: ";
+			cin >> matrix[i][j];
+		}
+		cout << endl;
+	}
+
+	// print matrix
+	for (size_t i = 0; i < n; i++)
+	{
+		for (size_t j = 0; j < m; j++)
+		{
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	// find max value
+	for (size_t i = 0; i < n; i++)
+	{
+		for (size_t j = i + 1; j < m; j++)
+		{
+			cout << matrix[i][j] << " ";
+			if (matrix[i][j] > maxValue)
+				maxValue = matrix[i][j];
+		}
+	}
+	cout << endl;
+
+	// print max value
+	cout << "Max value: " << maxValue << endl;
 }
 
 int main()
 {
-	task1();
-	// task2();
+	//task1();
+	task2();
 }
