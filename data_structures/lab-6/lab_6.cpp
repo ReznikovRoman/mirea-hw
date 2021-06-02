82d18ff272cf68446d5b8f3a84b9fe9610e96b98
ReznikovRoman <romanreznikov2002@yandex.ru>
2021-06-02 15:04:51 +0300

    Add source code for 'lab-6'

--- data_structures/lab-6/lab_6.cpp
+++ data_structures/lab-6/lab_6.cpp
@@ -3,158 +3,89 @@
 #include <stack>
 using namespace std;
 
-// Задание 1
-// Получаем приоритет операторов
-int getOperatorsPrecedence(char c)
+// 1. Рекурсивный бинарный поиск
+int binarySearch(int arr[], int left, int right, int key)
 {
-    if (c == '^')
-        return 3;
-    else if (c == '*' || c == '/')
-        return 2;
-    else if (c == '+' || c == '-')
-        return 1;
-    else
-        return -1;
-}
-
-// Конвертируем инфиксную форму в постфиксную
-string infixToPostfix(string s)
-{
-    stack<char> operationsStack;
-    string result;
-
-    for (int i = 0; i < s.length(); i++)
+    if (right >= left)
     {
-        char c = s[i];
+        // "середина" массива
+        int middle = left + (right - left) / 2;
 
-        // Добавляем операнды к результату
-        if ((c >= 'a' && c <= 'z') ||
-            (c >= 'A' && c <= 'Z') ||
-            (c >= '0' && c <= '9'))
-        {
-            result += c;
-        }
+        if (arr[middle] == key)
+            return middle;
 
-        // "Обработка скобок"
-        else if (c == '(')
-            operationsStack.push('(');
+        // Если элемент меньше середины, то ищем в левой части массива
+        if (arr[middle] > key)
+            return binarySearch(arr, left, middle - 1, key);
 
-        else if (c == ')')
-        {
-            while (!operationsStack.empty() && operationsStack.top() != '(')
-            {
-                char temp = operationsStack.top();
-                operationsStack.pop();
-                result += temp;
-            }
-            operationsStack.pop();
-        }
-        else
-        {
-            while (!operationsStack.empty() &&
-                   getOperatorsPrecedence(s[i]) < getOperatorsPrecedence(operationsStack.top()))
-            {
-                char temp = operationsStack.top();
-                operationsStack.pop();
-                result += temp;
-            }
-            operationsStack.push(c);
-        }
+        // Иначе - ищем во второй
+        return binarySearch(arr, middle + 1, right, key);
     }
 
-    // Убираем оставшиеся элементы из стэка
-    while (!operationsStack.empty())
-    {
-        char temp = operationsStack.top();
-        operationsStack.pop();
-        result += temp;
-    }
-
-    return result;
+    // Элемента в массиве нет
+    return -1;
 }
 
-// Задание - 2 (стек реализован с помощью списка)
-struct StackNode
+// 2. Создание двунаправленного списка (без рекурсии ?)
+struct Node
 {
     int data;
-    StackNode *next;
+    Node *next;
+    Node *prev;
 };
 
-StackNode *newNode(int data)
+// Вставить новую ноду в начало списка
+void push(Node **headRef, int newData)
 {
-    StackNode *stackNode = new StackNode();
-    stackNode->data = data;
-    stackNode->next = NULL;
-    return stackNode;
+    Node *newNode = new Node();
+    newNode->data = newData;
+
+    newNode->next = (*headRef);
+    newNode->prev = NULL;
+
+    // Переставляем "предыдущий" указатель на добавленную ноду
+    if ((*headRef) != NULL)
+    {
+        (*headRef)->prev = newNode;
+    }
+
+    // Переставляем указатель на добавленную ноду
+    (*headRef) = newNode;
 }
 
-// 2 - "Пуст ли стек"
-int isEmpty(StackNode *root)
+// Вывод двунаправленного списка
+void printList(Node *node)
 {
-    return !root;
-}
-
-// 2 - "Воткнуть элемент в стек"
-void push(StackNode **root, int data)
-{
-    StackNode *stackNode = newNode(data);
-    stackNode->next = *root;
-    *root = stackNode;
-    cout << data << " pushed to stack\n";
-}
-
-// 2 - "Вытолкнуть элемент из стек"
-int pop(StackNode **root)
-{
-    if (isEmpty(*root))
-        return INT_MIN;
-    StackNode *temp = *root;
-    *root = (*root)->next;
-    int popped = temp->data;
-    free(temp);
-
-    return popped;
-}
-
-// 2 - "Вернуть значение вершины стека"
-int peek(StackNode *root)
-{
-    if (isEmpty(root))
-        return INT_MIN;
-    return root->data;
+    cout << "Iterate in forward direction \n";
+    while (node != NULL)
+    {
+        cout << " " << node->data << " ";
+        node = node->next;
+    }
 }
 
 void task1()
 {
-    string exp = "((a+b)*c)";
+    int arr[] = {2, 3, 4, 10, 40};
+    int key = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, 0, n - 1, key);
 
-    cout << infixToPostfix(exp) << endl; // ab + c*
+    cout << "Index of `" << key << "`: " << result << endl;
 }
 
 void task2()
 {
-    StackNode *root = NULL;
+    Node *head = NULL;
 
-    push(&root, 1);
-    push(&root, 2);
-    push(&root, 3);
+    push(&head, 7);
+    push(&head, 1);
 
-    cout << pop(&root) << " has been popped from stack\n";
-
-    cout << "Top element is " << peek(root) << endl;
-
-    cout << "Elements in stack : ";
-    while (!isEmpty(root))
-    {
-        // Выводим значение "верхнего" элемента
-        cout << peek(root) << " ";
-
-        // Удаляем значение "верхнего" элемента
-        pop(&root);
-    }
+    cout << "List items: ";
+    printList(head);
 }
 
-int main()
+int main(void)
 {
     task1();
     cout << endl;
